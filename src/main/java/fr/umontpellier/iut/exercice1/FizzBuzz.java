881d5fa1d64c21414ecc42e533242c76dc7378ac
Yacin-the <yacine.oubrahim@etu.umontpellier.fr>
2020-01-28 17:21:22 +0100

    Etape_returns_fizz_when_three

--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -3,7 +3,6 @@ package fr.umontpellier.iut.exercice1;
 public class FizzBuzz {
 
     public String getValue(int i) {
-        if(i==2) return "2";
         if(i==3) return "Fizz";
         return String.valueOf(i);
     }
